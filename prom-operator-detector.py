7c8eb840e8c5130f57811da6e7b1175c25d7d902
Pavan Gudiwada <25551553+pavangudiwada@users.noreply.github.com>
2023-12-28 10:58:57 +0530

    Updated with extended functionality

--- prom-operator-detector.py
+++ prom-operator-detector.py
@@ -1,18 +1,25 @@
 from kubernetes import client, config
-
-rule_selectors = {} # Collects all the Prometheus Operators and their rule selectors as a dictonary with (namespace, name), a tuple as the key
+import time
+import streamlit as st
+from streamlit import session_state as ss
 
 def list_all_namespaces():
+
+    # st.toast("Detected K8s Cluster", icon='☸️')
 
     config.load_kube_config()
     v1 = client.CoreV1Api()
     namespaces = v1.list_namespace() # Get all the namespaces
+
+    ss.cluster_detected = True
 
     return [namespace.metadata.name for namespace in namespaces.items] # For each namespaces in the list of namespaces get the name found in metadata.name
 
 
 def get_prometheus_rule_selector(namespaces):
     custom_objects_api = client.CustomObjectsApi()
+    rule_selectors = {} # Collects all the Prometheus Operators and their rule selectors as a set (namespace, name)
+    rule_namespace_selector = {}
 
     for namespace in namespaces: # Get the list of namespaces and try to find a prometheus operator 
         try: # This is like running kubectl get prometheuses
@@ -24,18 +31,36 @@ def get_prometheus_rule_selector(namespaces):
             )
 
             for item in prometheus_operators['items']: # There could be multiple operators, and items has a list of them
-                print(item)
                 name = item['metadata']['name']        # From the metadata part of each item, get its name
                 rule_selector = item.get('spec', {}).get('ruleSelector', {})   # If spec or ruleSelector doesnt exist then {} is used instead of an exception
-                rule_selectors[(namespace, name)] = rule_selector     # If there was a rule add it to the rule_selector dictonary
+                rule_selectors[(namespace, name)] = rule_selector     # If there was a rule 
+
+                rule_namespace = item.get('spec', {}).get('ruleNamespaceSelector', {})
+                rule_namespace_selector[(namespace, name)] = rule_namespace
 
         except client.exceptions.ApiException as e:
             print(f"An error occurred in namespace {namespace}: {e}")
+    # st.toast("Prometheus Operator Detected", icon="👌")
 
-    return rule_selectors
+    return rule_selectors, rule_namespace_selector
 
-namespaces = list_all_namespaces()
+# namespaces = list_all_namespaces()
 
-rule_selectors = get_prometheus_rule_selector(namespaces)
-for (namespace, name), selector in rule_selectors.items():
-    print(f"Namespace: {namespace}, Operator: {name}, Rule Selector: {selector}")
+# rule_selectors = get_prometheus_rule_selector(namespaces)
+# for (namespace, name), selector in rule_selectors.items():
+#     print(f"Namespace: {namespace}, Operator: {name}, Rule Selector: {selector}")
+
+# def get_current_cluster_name():
+#     # Load the kubeconfig file
+#     config.load_kube_config()  # Use config.load_incluster_config() if running inside a cluster
+
+#     # Get the current context
+#     current_context = config.list_kube_config_contexts()[1]['context']
+
+#     # The name of the current cluster
+#     cluster_name = current_context['cluster']
+#     return cluster_name
+
+# # Example usage
+# cluster_name = get_current_cluster_name()
+# print(cluster_name)
